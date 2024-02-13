45003b93476025718a11040321a0bc739142799a
vulpyrin <alexanderguerrero@sandiego.edu>
2024-02-13 15:43:29 -0800

    Added Swift files, and got pop up working

---
+++ obstacle_avoidance1/obstacle_avoidanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  obstacle_avoidanceApp.swift
+//  obstacle_avoidance
+//
+//  Created by Alexander on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct obstacle_avoidanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
