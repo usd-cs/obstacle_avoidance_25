729aedfa887d25e1ec632ded08e6c8b382f79ef0
Kenny Collins <kennethcollins@sandiego.edu>
2024-02-15 11:47:55 -0800

    Adding some pretrained models

--- computer-vision/scripts/yv8_training.py
+++ computer-vision/scripts/yv8_training.py
@@ -1,3 +1,8 @@
 from ultralytics import YOLO
+'''
+Super Annoying but need python3.10 for this one thing.
+coreml has nonstop updates, and we need version 7.1 to export trained models to coreml
+'''
+model = YOLO('computer-vision/models/yolov8n-oiv7.pt')
 
-model = YOLO('')
+model.export(format='coreml')
