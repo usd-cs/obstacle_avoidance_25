b8151588824fca0599b6afa69d517b2d59f75685
Scott Schnieders <atsimcode@gmail.com>
2024-02-28 20:00:43 -0800

    Wrote the AudioQueue object class with error checking for invalid data

---
+++ computer-vision/decision/AudioQueue.swift
@@ -0,0 +1,32 @@
+/* 
+An object class to store the audio queues sent from the DecisionBlock to the UI.
+
+Author: Scott Schnieders
+Last modfiied: 2/28/2024
+
+ */
+enum AudioQueueError: Error {
+    case invalidThreatLevel
+    case invalidAngle
+}
+
+class AudioQueue {
+    var threatLevel: Int // Threat level of the object between 0-100, with 100 being the greatest threat.
+    var objectName: String // Name of the object
+    var angle: Int // Angle of the object in clock terms. Ex. 12 O'clock would be straight forward.
+    var distance: Int // Distance calculated from the person holding phone to the object (in feet).
+    
+    init(threatLevel: Int, objectName: String, angle: Int, distance: Int) throws {
+        guard threatLevel >= 0 && threatLevel <= 100 else {
+            throw AudioQueueError.invalidThreatLevel
+        }
+        guard [9,10,11,12,1,2,3].contains(angle) else {
+            throw AudioQueueError.invalidAngle
+        }
+        
+        self.threatLevel = threatLevel
+        self.objectName = objectName
+        self.angle = angle
+        self.distance = distance
+    }
+}
