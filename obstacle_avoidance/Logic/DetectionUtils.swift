bd1a2604eaaa18d376aa5f5cd9991e1b4928f9bb
CBreach <cbreach@sandiego.edu>
2025-04-07 12:33:55 -0700

    created the barebones version of PolartoCartesian as well as it's unit teting file

--- obstacle_avoidance/Logic/DetectionUtils.swift
+++ obstacle_avoidance/Logic/DetectionUtils.swift
@@ -27,4 +27,23 @@ struct DetectionUtils {
         let index = min(Int(percentage/33.33), section.count-1)
         return section[index]
     }
+    /*
+     this functio is what handles the conversion from polar chords to Cardinal cords.
+     - this will be specially useful for our corridor idea.
+     - we take in the distance (from LiDar sensor) as well as the angle of the detected obstacle
+        - ps: they are not necesarilly floats the types will be adjusted accordingly this is just the skeleton of the function
+     */
+    static func polarToCartesian(distance: Float, angle: Float) -> (Float, Float){
+        if distance <= 0 || angle <= 0 {return (-1,-1)} //this shoudl only happen in case of an error in either distance or angle calculation.
+        //we need to first convert the angle to radians
+        let angleRadians = (angle * .pi) / 180
+        var xCord = distance * cos(angleRadians)
+        var yCord = distance * sin(angleRadians)
+
+        //swift is dumb and does not have a built in round func to a given decimal so we'll have ot
+        //work around it
+        xCord = Float(round(10 * xCord) / 10)
+        yCord = Float(round(10 * yCord) / 10)
+        return (x: xCord,y: yCord)
+    }
 }
