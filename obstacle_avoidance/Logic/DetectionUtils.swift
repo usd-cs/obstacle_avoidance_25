4cb3d4a758660ae7b3af3b61e6df9490a998c2bd
CBreach <cbreach@sandiego.edu>
2025-04-08 21:09:03 -0700

    latest changes in frame handler, beginning of the corridor implementation idea as well as a fix to the direction function

--- obstacle_avoidance/Logic/DetectionUtils.swift
+++ obstacle_avoidance/Logic/DetectionUtils.swift
@@ -13,10 +13,10 @@ struct DetectionUtils {
         guard percentage >= 0, percentage <= 100 else { return "Unknown" }
 
         let directions = [
-            "9 o'clock", "10 o'clock", "11 o'clock",
+            "10 o'clock", "11 o'clock",
             "12 o'clock", "1 o'clock", "2 o'clock"
         ]
-        let index = min(Int(percentage / 16.67), directions.count - 1)
+        let index = min(Int(percentage / 20.0), directions.count - 1)
         return directions[index]
     }
     static func calculateScreenSection(_ percentage:CGFloat) -> String{
@@ -27,13 +27,27 @@ struct DetectionUtils {
         let index = min(Int(percentage/33.33), section.count-1)
         return section[index]
     }
+    static func directionToDegrees(direction: String) -> Float {
+
+        let degrees = [
+            "10 o'clock" : 0,
+            "11 o'clock" : 20,
+            "12 o'clock" : 40,
+            "1 o'clock" : 60,
+            "2 o'clock" : 80
+        ]
+
+        return Float(degrees[direction]!)
+    }
     /*
      this functio is what handles the conversion from polar chords to Cardinal cords.
      - this will be specially useful for our corridor idea.
      - we take in the distance (from LiDar sensor) as well as the angle of the detected obstacle
         - ps: they are not necesarilly floats the types will be adjusted accordingly this is just the skeleton of the function
      */
-    static func polarToCartesian(distance: Float, angle: Float) -> (Float, Float){
+    static func polarToCartesian(distance: Float, direction: String) -> (Float, Float){
+
+        let angle = directionToDegrees(direction: direction)
         if distance <= 0 || angle <= 0 {return (-1,-1)} //this shoudl only happen in case of an error in either distance or angle calculation.
         //we need to first convert the angle to radians
         let angleRadians = (angle * .pi) / 180
@@ -42,8 +56,8 @@ struct DetectionUtils {
 
         //swift is dumb and does not have a built in round func to a given decimal so we'll have ot
         //work around it
-        xCord = Float(round(10 * xCord) / 10)
-        yCord = Float(round(10 * yCord) / 10)
+        xCord = Float(round(100 * xCord) / 100)
+        yCord = Float(round(100 * yCord) / 100)
         return (x: xCord,y: yCord)
     }
 }
